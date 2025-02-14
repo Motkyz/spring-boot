b9f3da63320fe42b65bfa69f9ad6341bf9ac2be7
MotKyz <kuznetsov.matvey.2005@mail.ru>
2025-02-14 20:17:02 +0500

    new post view + datetime

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,19 +1,29 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text, Integer likes) {
+    public Post(String text, Date creationDate, Integer likes) {
         this.text = text;
         this.likes = likes;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
+
         return text;
     }
 
     public Integer getLikes() {
+
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +11,9 @@ import java.util.List;
 public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<Post>();
-        posts.add(new Post("first", 5));
-        posts.add(new Post("second", 7));
-        posts.add(new Post("third", 8));
+        posts.add(new Post("first", new Date(),5));
+        posts.add(new Post("second", new Date(), 7));
+        posts.add(new Post("third", new Date(), 8));
         return posts;
     }
 }
